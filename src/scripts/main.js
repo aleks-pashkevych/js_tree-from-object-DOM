13b9fb24805c8361507874e66f078a7549cdd658
Aleks Pashkevych <alekspashkevych@gmail.com>
2025-12-25 05:16:48 +0200

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,33 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
+let tree = document.querySelector('#tree');
+
+if (tree === null) {
+  tree = document.createElement('div');
+
+  const body = document.querySelector('body');
+
+  body.appendChild(tree);
+}
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+
+  if (Object.keys(data).length === 0) {
+    return false;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    createTree(li, data[key]);
+    ul.appendChild(li);
+  }
+  element.append(ul);
 }
 
 createTree(tree, food);
